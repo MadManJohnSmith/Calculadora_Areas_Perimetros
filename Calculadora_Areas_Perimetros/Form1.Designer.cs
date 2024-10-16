e5075c7f4c437d527f0af757f68ca61af28614c6
ALAN SALAS PARADA <alan.salasp@alumno.buap.mx>
2024-10-16 00:48:59 -0600

    Se crea Boceto de Interfaz Grafica

--- Calculadora_Areas_Perimetros/Form1.Designer.cs
+++ Calculadora_Areas_Perimetros/Form1.Designer.cs
@@ -28,20 +28,263 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnHeptagon = new System.Windows.Forms.Button();
+            this.btnTrapezoid = new System.Windows.Forms.Button();
+            this.btnTriangle = new System.Windows.Forms.Button();
+            this.btnCircle = new System.Windows.Forms.Button();
+            this.btnSquare = new System.Windows.Forms.Button();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tableLayoutPanel1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(135, 661);
+            this.panel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.btnTrapezoid, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.btnTriangle, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.btnSquare, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnCircle, 0, 4);
+            this.tableLayoutPanel1.Controls.Add(this.btnHeptagon, 0, 3);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.tableLayoutPanel1.GrowStyle = System.Windows.Forms.TableLayoutPanelGrowStyle.FixedSize;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 5;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(135, 661);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnHeptagon
+            // 
+            this.btnHeptagon.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnHeptagon.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnHeptagon.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnHeptagon.FlatAppearance.BorderSize = 0;
+            this.btnHeptagon.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnHeptagon.Font = new System.Drawing.Font("Product Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHeptagon.ForeColor = System.Drawing.Color.Teal;
+            this.btnHeptagon.Image = global::Calculadora_Areas_Perimetros.Properties.Resources.area_del_heptagono_100x98;
+            this.btnHeptagon.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnHeptagon.Location = new System.Drawing.Point(0, 396);
+            this.btnHeptagon.Margin = new System.Windows.Forms.Padding(0);
+            this.btnHeptagon.Name = "btnHeptagon";
+            this.btnHeptagon.Size = new System.Drawing.Size(135, 132);
+            this.btnHeptagon.TabIndex = 4;
+            this.btnHeptagon.Text = "Heptágono";
+            this.btnHeptagon.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnHeptagon.UseVisualStyleBackColor = false;
+            this.btnHeptagon.Click += new System.EventHandler(this.btnHeptagon_Click);
+            // 
+            // btnTrapezoid
+            // 
+            this.btnTrapezoid.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTrapezoid.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTrapezoid.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnTrapezoid.FlatAppearance.BorderSize = 0;
+            this.btnTrapezoid.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTrapezoid.Font = new System.Drawing.Font("Product Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTrapezoid.ForeColor = System.Drawing.Color.Teal;
+            this.btnTrapezoid.Image = global::Calculadora_Areas_Perimetros.Properties.Resources.area_del_trapezoide_100x100;
+            this.btnTrapezoid.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTrapezoid.Location = new System.Drawing.Point(0, 264);
+            this.btnTrapezoid.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTrapezoid.Name = "btnTrapezoid";
+            this.btnTrapezoid.Size = new System.Drawing.Size(135, 132);
+            this.btnTrapezoid.TabIndex = 2;
+            this.btnTrapezoid.Text = "Trapezoide";
+            this.btnTrapezoid.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnTrapezoid.UseVisualStyleBackColor = false;
+            this.btnTrapezoid.Click += new System.EventHandler(this.btnTrapezoid_Click);
+            // 
+            // btnTriangle
+            // 
+            this.btnTriangle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnTriangle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnTriangle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnTriangle.FlatAppearance.BorderSize = 0;
+            this.btnTriangle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnTriangle.Font = new System.Drawing.Font("Product Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTriangle.ForeColor = System.Drawing.Color.Teal;
+            this.btnTriangle.Image = global::Calculadora_Areas_Perimetros.Properties.Resources.area_del_triangulo_100x87;
+            this.btnTriangle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnTriangle.Location = new System.Drawing.Point(0, 0);
+            this.btnTriangle.Margin = new System.Windows.Forms.Padding(0);
+            this.btnTriangle.Name = "btnTriangle";
+            this.btnTriangle.Size = new System.Drawing.Size(135, 132);
+            this.btnTriangle.TabIndex = 3;
+            this.btnTriangle.Text = "Triángulo";
+            this.btnTriangle.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnTriangle.UseVisualStyleBackColor = false;
+            this.btnTriangle.Click += new System.EventHandler(this.btnTriangle_Click);
+            // 
+            // btnCircle
+            // 
+            this.btnCircle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnCircle.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCircle.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnCircle.FlatAppearance.BorderSize = 0;
+            this.btnCircle.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnCircle.Font = new System.Drawing.Font("Product Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCircle.ForeColor = System.Drawing.Color.Teal;
+            this.btnCircle.Image = global::Calculadora_Areas_Perimetros.Properties.Resources.area_del_circulo_100x100;
+            this.btnCircle.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnCircle.Location = new System.Drawing.Point(0, 528);
+            this.btnCircle.Margin = new System.Windows.Forms.Padding(0);
+            this.btnCircle.Name = "btnCircle";
+            this.btnCircle.Size = new System.Drawing.Size(135, 133);
+            this.btnCircle.TabIndex = 1;
+            this.btnCircle.Text = "Circulo";
+            this.btnCircle.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnCircle.UseVisualStyleBackColor = false;
+            this.btnCircle.Click += new System.EventHandler(this.btnCircle_Click);
+            // 
+            // btnSquare
+            // 
+            this.btnSquare.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnSquare.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnSquare.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnSquare.FlatAppearance.BorderSize = 0;
+            this.btnSquare.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSquare.Font = new System.Drawing.Font("Product Sans", 14.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSquare.ForeColor = System.Drawing.Color.Teal;
+            this.btnSquare.Image = global::Calculadora_Areas_Perimetros.Properties.Resources.area_del_cuadrado_100x100;
+            this.btnSquare.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnSquare.Location = new System.Drawing.Point(0, 132);
+            this.btnSquare.Margin = new System.Windows.Forms.Padding(0);
+            this.btnSquare.Name = "btnSquare";
+            this.btnSquare.Size = new System.Drawing.Size(135, 132);
+            this.btnSquare.TabIndex = 0;
+            this.btnSquare.Text = "Cuadrado";
+            this.btnSquare.TextAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.btnSquare.UseVisualStyleBackColor = false;
+            this.btnSquare.Click += new System.EventHandler(this.btnSquare_Click);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.panel2.Controls.Add(this.linkLabel1);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(135, 0);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(689, 661);
+            this.panel2.TabIndex = 1;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkBehavior = System.Windows.Forms.LinkBehavior.HoverUnderline;
+            this.linkLabel1.Location = new System.Drawing.Point(278, 639);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(340, 13);
+            this.linkLabel1.TabIndex = 3;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/MadManJohnSmith/Calculadora_Areas_Perimetros";
+            this.linkLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Font = new System.Drawing.Font("Product Sans", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(38, 193);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(611, 366);
+            this.label3.TabIndex = 2;
+            this.label3.Text = resources.GetString("label3.Text");
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(624, 639);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ver. 1.0.0";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.Font = new System.Drawing.Font("ROG Fonts", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(686, 180);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calculadora de Areas y Perimetros";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 450);
+            this.ClientSize = new System.Drawing.Size(824, 661);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(840, 700);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button btnHeptagon;
+        private System.Windows.Forms.Button btnTriangle;
+        private System.Windows.Forms.Button btnTrapezoid;
+        private System.Windows.Forms.Button btnCircle;
+        private System.Windows.Forms.Button btnSquare;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.LinkLabel linkLabel1;
     }
 }
 
--- Calculadora_Areas_Perimetros/Form1.cs
+++ Calculadora_Areas_Perimetros/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Calculadora_Areas_Perimetros.UserControls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,60 @@ namespace Calculadora_Areas_Perimetros
 {
     public partial class Form1 : Form
     {
+        NavigationControl navigationControl;
+        NavigationButtons navigationButtons;
+        // Change the color of your buttons if you want
+        Color btnDefaultColor = Color.FromKnownColor(KnownColor.ControlLight);
+        Color btnSelectedtColor = Color.FromKnownColor(KnownColor.ControlDark);
         public Form1()
         {
             InitializeComponent();
+            InitializeNavigationControl();
+            InitializeNavigationButtons();
+        }
+        private void InitializeNavigationControl()
+        {
+            List<UserControl> userControls = new List<UserControl>() // Your UserControl list
+            { new Circle(), new Heptagon(), new Squaer__(), new Trapezoid(),new Triangle()};
+
+            navigationControl = new NavigationControl(userControls, panel2); 
+        }
+
+        private void InitializeNavigationButtons()
+        {
+            List<Button> buttons = new List<Button>()
+            { btnTriangle, btnSquare, btnTrapezoid, btnCircle, btnHeptagon };
+            navigationButtons = new NavigationButtons(buttons, btnDefaultColor, btnSelectedtColor);
+        }
+
+        private void btnTriangle_Click(object sender, EventArgs e)
+        {
+            navigationControl.Display(4);
+            navigationButtons.Highlight(btnTriangle);
+        }
+
+        private void btnSquare_Click(object sender, EventArgs e)
+        {
+            navigationControl.Display(2);
+            navigationButtons.Highlight(btnSquare);
+        }
+
+        private void btnTrapezoid_Click(object sender, EventArgs e)
+        {
+            navigationControl.Display(3);
+            navigationButtons.Highlight(btnTrapezoid);
+        }
+
+        private void btnCircle_Click(object sender, EventArgs e)
+        {
+            navigationControl.Display(0);
+            navigationButtons.Highlight(btnCircle);
+        }
+
+        private void btnHeptagon_Click(object sender, EventArgs e)
+        {
+            navigationControl.Display(1);
+            navigationButtons.Highlight(btnHeptagon);
         }
     }
 }
